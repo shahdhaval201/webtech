4876c9ee8d5b1b0a12317ba002dce17b8467dc11
Dhaval201 <shahdhaval201@gmail.com>
2022-09-14 02:54:53 +0530

    Interview task update

--- src/Component/EmployeeDetails.js
+++ src/Component/EmployeeDetails.js
@@ -289,43 +289,10 @@ const EmployeeDetails = () => {
                                 })}
                               </tbody>
                             </table>
-                            {/* 
-                            <button type="button" onClick={handleSavePdf}>
-                              Download
-                            </button> */}
                           </div>
                           <button type="button" onClick={handleSavePdf}>
                             Download
                           </button>
-                          {/* <div>
-                            <Document
-                              file="somefile.pdf"
-                              onLoadSuccess={DownloadPdf}
-                            >
-                              <Page pageNumber={pageNumber} />
-                            </Document>
-                            <p>
-                              Page {pageNumber} of {numPages}
-                            </p>
-                          </div> */}
-                          {/* <h2 className="fs-title text-center">Success !</h2>
-                          <br />
-                          <br />
-                          <div className="row justify-content-center">
-                            <div className="col-3">
-                              <img
-                                src="https://img.icons8.com/color/96/000000/ok--v2.png"
-                                className="fit-image"
-                              />
-                            </div>
-                          </div>
-                          <br />
-                          <br />
-                          <div className="row justify-content-center">
-                            <div className="col-7 text-center">
-                              <h5>You Have Successfully Signed Up</h5>
-                            </div>
-                          </div> */}
                         </div>
                       </fieldset>
                     </form>
